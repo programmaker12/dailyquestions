c3d852709f2403957921d6ca22c30a4e3f90cb8a
programmaker12 <139685764+programmaker12@users.noreply.github.com>
2024-05-01 18:49:06 +0530

    Update Jailer.java

--- Day3/Jailer.java
+++ Day3/Jailer.java
@@ -1,20 +1,20 @@
 /*
- *                   Prisoners	vs	Jailer!			
- *  A jail	has	a number of	prisoners and a	number of treats	
- *  to pass out	to them. Their	jailer	decides	the	fairest	way	to	divide	the	treats is to seat the prisoners	
- *   around a circular table	in	sequentially numbered chairs. A	chair number will be drawn from a hat.	
- *   Beginning with	the	prisoner in	that chair,	one	candy will be handed to	each prisoner sequentially	around	the	table	
- *   until all have	been	distributed. The	jailer	is	playing	a	little	joke,	though.	The	last	piece	of	candy	looks	like	all	the	
- *   others,	but	it	tastes	awful. Determine	the	chair	number	occupied	by	the	prisoner	who	will	receive	that	
+ *                   Prisoners	vs Jailer!			
+ *  A jail has a number of prisoners and a number of treats	
+ *  to pass out	to them. Their	jailer	decides	the fairest way	to divide the treats is to seat the prisoners	
+ *   around a circular table in	sequentially numbered chairs. A	chair number will be drawn from a hat.	
+ *   Beginning with the	prisoner in that chair, one candy will be handed to each prisoner sequentially around the table	
+ *   until all have been distributed. The jailer is playing a little joke, though.  The	last piece of candy looks like all the	
+ *   others, but it tastes awful. Determine the	chair number occupied by the prisoner who will	receive	that	
  *   candy.				                 
  *   
- *   Input	Format	
- *  The	first line contains	an	integer, t,	denoting the number	of test cases.	
- *  The	next t lines each contain 3	space-separated	integers:- n : the	number	of	prisoners m	: the number of	sweets, s :	the	chair number to	start passing out treats at	
+ *   Input Format	
+ *  The	first line contains an	integer, t, denoting the number	of test cases.	
+ *  The	next t lines each contain 3 space-separated integers:- n : the	number	of prisoners m	: the number of	sweets, s : the	chair number to	start passing out treats at	
  *  
  *  Output Format	
- *  For	each test case,	print the chair	number of the prisoner who	receives the awful	
- *  treat on	a new line.	
+ *  For	each test case,	print the chair	number of the prisoner who receives the awful	
+ *  treat on a new line.	
  *                   
  *                   
  */
