292dd59d55ae02f73ed3c01465af98b9a0f3a2ab
programmaker12 <139685764+programmaker12@users.noreply.github.com>
2024-05-01 18:52:54 +0530

    Update SetBit.java

--- Day4/SetBit.java
+++ Day4/SetBit.java
@@ -2,20 +2,19 @@
  *                   Set Bit calculator 
  *                   
  *                   
- * Angela plays with the different bits. She was given a bunch of 
-numbers and she needs to find how many set bits are there in total. 
-Help Angela to impress her, write a code to do so.
-
-Input Format:
- •First line with n, an integer
- •Next n lines denoting n integers angela is given.
- 
- Output Format:
- •Total number of set bits
- 
- Constraints:
- •Number of elements or number <=10^7
- •numbers<=10000
+ *  Angela plays with the different bits. She was given a bunch of 
+ *  numbers and she needs to find how many set bits are there in total. 
+ *  Help Angela to impress her, write a code to do so.
+ *
+ * Input Format:
+ * •First line with n, an integer
+ * •Next n lines denoting n integers angela is given.
+ * *Output Format:
+ * •Total number of set bits
+ *
+ * Constraints:
+ * •Number of elements or number <=10^7
+ * •numbers<=10000
  */
 package Day4;
 import java.util.Scanner;
