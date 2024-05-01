3033053c8c73b234c19c209adb2384e8fdff7d9d
programmaker12 <139685764+programmaker12@users.noreply.github.com>
2024-05-01 18:45:26 +0530

    Update DiskSpaceAnalysis.java

--- Day2/DiskSpaceAnalysis.java
+++ Day2/DiskSpaceAnalysis.java
@@ -1,3 +1,26 @@
+/*
+ *                          Disk Space Analysis
+ *  Problem Statement -: 
+ *  You are given an array, 
+ *  You have to choose a contiguous subarray of length ‘k’, and 
+ *  find the minimum of that segment, 
+ *  return the maximum of those minimums.
+ *  
+ *  INPUT AND OUTPUT
+ *  
+ *   Sample input  
+ *    1 →  Length of segment x =1
+ *    5 →  size of space n = 5
+ *    1 → space = [ 1,2,3,1,2]
+ *    2 
+ *    3
+ *    1
+ *    2
+ *    
+ *   Sample output
+ *    3
+ *
+ */
 package Day2;
 import java.util.Scanner;
 import java.util.*;
