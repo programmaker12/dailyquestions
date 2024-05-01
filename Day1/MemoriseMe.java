565acabd348452074b32ff929270553fed4c1b7c
programmaker12 <139685764+programmaker12@users.noreply.github.com>
2024-05-01 18:31:56 +0530

    Update MemoriseMe.java

--- Day1/MemoriseMe.java
+++ Day1/MemoriseMe.java
@@ -1,3 +1,35 @@
+/*
+ *                 Minimum Occurrence
+ *    Problem Statement -: 
+ *    Arijit is a brilliant boy. He likes memory games. He likes to participate alone
+ *    but this time he has to have a partner. So he chooses you.
+ *    Inthis Game , your team will be shown N numbers for few minutes . You will
+ *    have to memorize these numbers
+ *      
+ *    Now, the questioner will ask you Q queries, in each query He will give you a
+ *    number , and you have to tell him the total number of occurrences of that
+ *    number in the array of numbers shown to your team .
+ *    If the number is not present , then you will have to say “NOT PRESENT”
+ *    (without quotes).            
+ *           
+ *    Input Format:
+ *    The first line of input will contain N, an integer, which is the total number of
+ *    numbers shown to your team.
+ *    The second line of input contains N space separated integers .
+ *    The third line of input contains an integer Q , denoting the total number of
+ *    integers.(testcases)
+ *    The Next Q lines will contain an integer denoting an integer, (Bi) , for which you
+ *    must print the number of occurrences of that number (Bi) in those N numbers on
+ *    a new line.
+ *    If the number (Bi) isn’t present, then Print “NOT PRESENT” (without quotes) on a
+ *    new line.
+ *    
+ *    Constraints:
+ *    Length of string <=10^6
+ *    
+ *    Sample output
+ *    3
+ */
 package Day1;
 import java.util.Scanner;
 public class MemoriseMe {
