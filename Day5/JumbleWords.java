5379f22f218e3880354b363b268f6ec974095cfc
programmaker12 <139685764+programmaker12@users.noreply.github.com>
2024-05-03 18:02:36 +0530

    Day 5

---
+++ Day5/JumbleWords.java
@@ -0,0 +1,32 @@
+package Day5;
+import java.util.Scanner;
+public class JumbleWords {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Please enter first word");
+        String s1, s2, jword = " ";
+        int i;
+        Scanner sc = new Scanner(System.in);
+        s1 = sc.next();
+        System.out.println("Please enter first word");
+        s2 = sc.next();
+        //Loop for jumbling the word
+        for(i =0; i<s1.length() && i < s2.length(); i++)
+        {
+        	jword+=s1.charAt(i);
+        	jword+=s2.charAt(i);
+        }
+        // Condition for attaching remaining leeters in a word
+        if(s1.length() > s2.length())
+        {
+        	jword+=s1.substring(i);
+        }
+        else if(s2.length() > s1.length())
+        {
+        	jword+=s2.substring(i);
+        }
+        System.out.println("The jumble word is"+jword);
+	}
+
+}
---
+++ Day5/MaximumAttendence.java
@@ -0,0 +1,50 @@
+package Day5;
+import java.util.Scanner;
+public class MaximumAttendence {
+     // Function for finding the maximum number od consecutive days on which students are present
+static int MaximumAttendence(int n, String records[])
+{
+	String present = "";
+	int count = 0, max = 0;
+	for (int i = 0; i < n; i++) {
+		present+="P";
+		
+	}
+	System.out.println(present);
+	
+	for (int i = 0; i < records.length; i++) {
+		if (records[i].equals(present))
+		{
+			count++;
+		}
+		else if (count > max)
+		{
+			max = count;
+			count = 0;
+		}
+		
+	}
+	if (count > max)
+	{
+		max = count;
+	}
+	return max;
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter the number of students");
+		int n = sc.nextInt();
+		System.out.println("Please enter the number of days");
+		int m = sc.nextInt();
+		String [] records = new String [m];
+		for (int i = 0; i < records.length; i++) {
+			records[i] = sc.next();
+		}
+		
+		
+		System.out.println(MaximumAttendence(n, records));
+
+	}
+
+}
